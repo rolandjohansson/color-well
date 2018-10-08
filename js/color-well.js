1bed22df096b1bc0943dac7b9947111c5c326dc3
Roland Johansson <hello@rolandjohansson.se>
2018-10-08 13:38:04 +0200

    first commit

---
+++ js/color-well.js
@@ -0,0 +1,26 @@
+let colorWell;
+let defaultColor = "#0000ff";
+
+//När window laddas, kör funktionen startup
+window.addEventListener("load", startUp, false);
+
+function startUp() {
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateAll, false);
+  colorWell.addEventListener("change", updateFirst, false);
+  colorWell.select();
+}
+
+function updateFirst(event) {
+  let p = document.querySelector("p");
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
